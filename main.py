4f7d7529b9728789ac0d8ec96cc0f05478e74ecd
LeonardoRamazzotti <leonardo.ramazzotti95@gmail.com>
2021-09-10 00:43:45 +0200

    test button2 function. Add keyboard input for different desktop

--- main.py
+++ main.py
@@ -5,13 +5,38 @@ import subprocess
 import webbrowser
 import pandas as pd
 
-def Button_1():
-    loc = (r'automation_groups.xlsx')
+loc = (r'automation_groups.xlsx')
 
-    df = pd.read_excel(loc,sheet_name='Foglio1')
+def Button_1(excel_path):
+    
+    df = pd.read_excel(excel_path,sheet_name='Button1')
 
-    xl_key = df['key'[1]]
+    xl_key = list(df['actions'])
 
+
+def Button_2(excel_path):
+    
+    df = pd.read_excel(excel_path,sheet_name='Button2')
+
+    xl_key = list(df['actions'])
+
+    for element in xl_key:
+        if 'http' in element:
+            webbrowser.open(element)
+        else:
+            pass
+def Button_3(excel_path):
+    
+    df = pd.read_excel(excel_path,sheet_name='Button3')
+
+    xl_key = list(df['actions'])
+
+
+def Button_4(excel_path):
+    
+    df = pd.read_excel(excel_path,sheet_name='Button4')
+
+    xl_key = list(df['actions'])
 
 
 #GUI section
@@ -37,6 +62,9 @@ def clock():
     label_time.after(1000,clock)
 
 bg = PhotoImage(file='design.png')
+b1 = PhotoImage(file='study_icon.png')
+b2 = PhotoImage(file='work_icon.png')
+b3 = PhotoImage(file='streaming_icon.png')
 
 label_bg = Label(root,image=bg)
 label_bg.place(x=0,y=0,relwidth=1,relheight=1) 
@@ -45,6 +73,15 @@ label_bg.place(x=0,y=0,relwidth=1,relheight=1)
 
 clock()
 
+
+Button_study = Button(image=b1,bg='#333B41',command= Button_2(loc),borderwidth=0)
+Button_study.place(x=50,y=650)
+
+Button_streaming = Button(image=b3,bg='#333B41',command= Button_3,borderwidth=0)
+Button_streaming.place(x=250,y=650)
+
+Button_work = Button(image=b2,bg='#333B41',command= Button_3,borderwidth=0)
+Button_work.place(x=50,y=450)
 
 
 
